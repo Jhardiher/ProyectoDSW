d3df3dbdba939f9ea9b720febc83968fd8ded342
jhardiher <giraldojose814@gmail.com>
2024-05-31 14:57:32 -0500

    se corigio la page

--- ptyo-tienda-virtual/src/app/User/page.js
+++ ptyo-tienda-virtual/src/app/User/page.js
@@ -90,62 +90,66 @@ export default function UsuarioSesion() {
 
         </Box>
         <Box>
-          <Container classNameName="targetasPrincipal" >
-            <div classNameName="AreaTargetas">
-              <Card sx={{ margin: 2, minWidth: 275, maxWidth: 150 }}>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/comida1.svg"
-                  alt=""
-                />
-                <CardContent>
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    RECETAS FIT
-                  </Typography>
+      <Container className="targetasPrincipal" maxWidth="">
+        <div className="AreaTargetas">
+          <Card sx={{ margin: 2, minWidth: 275, maxWidth: 150 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/comida1.svg"
+              alt=""
+            />
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                RECETAS FIT
+              </Typography>
+              <Typography variant="h5" component="div">
+                SANA ALIMENTACION 
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Mejora tu IMC
+              </Typography>
+              <Typography variant="body2">
+                Siguenos
+                <br />
+                TU SALUD NUESTRA RESPONSABILIDAD
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button href="/RFood" size="small"> SABER MAS</Button>
+            </CardActions>
+          </Card>
 
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    Mejora tu IMC
-                  </Typography>
-                  <Typography variant="body2">
-                    Siguenos
-                    <br />
-                    {'"TU SALUD NUESTRA RESPONSABILIDAD"'}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button href="/RFood" size="small"> SABER MAS</Button>
-                </CardActions>
-              </Card>
-
-              <Card sx={{ margin: 2, minWidth: 275, maxWidth: 150 }}>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/rutinafit.svg "
-                  alt=""
-                />
-                <CardContent>
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    RUTINAS FIT
-                  </Typography>
-
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    Mejora tu IMC
-                  </Typography>
-                  <Typography variant="body2">
-                    Siguenos
-                    <br />
-                    {'"TU SALUD NUESTRA RESPONSABILIDAD"'}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">SABER MAS</Button>
-                </CardActions>
-              </Card>
-            </div>
-          </Container>
-        </Box>
+          <Card sx={{ margin: 2, minWidth: 275, maxWidth: 150 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/rutinafit.svg"
+              alt=""
+            />
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                RUTINAS FIT
+              </Typography>
+              <Typography variant="h5" component="div">
+                ENTRENA CUERPO Y ALMA
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Mejora tu IMC
+              </Typography>
+              <Typography variant="body2">
+                Siguenos
+                <br />
+                TU SALUD NUESTRA RESPONSABILIDAD
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">SABER MAS</Button>
+            </CardActions>
+          </Card>
+        </div>
+      </Container>
+    </Box>
 
         <section>
           Aquí iría el contenido de la tercera sección
